robertdfrench/zoolander-ci

commit 08095d124da6d38275ddcf937b7e828239467512

Consolidate infrastructure hierarchy into one directory (#52)
* Build from correct AMI in each region. Fixes #4
* Create a zoolander-ci record within the given hosted zone
* Consolidate infrastructure deployment. Add DNS. Fixes #25
* Remove comments from nginx configure
* Removes erroneous vpc config for eip. Fixes #54

infrastructure/Makefile
@@ -1,14 +1,12 @@
 include lib.mk
 
 terraform=$(call which, terraform)
+jq=$(call which, jq)
 
-install: $(terraform) .terraform/plugins/init zoolander.tf ##: Deploy latest infrastructure
+install: $(terraform) .terraform/plugins/init ##: Deploy latest infrastructure
 	$(call assertEnv, AWS_DEFAULT_REGION)
-	$(MAKE) -C image ami.json
-	cp image/ami.json ami.auto.tfvars.json
-	$(MAKE) -C dns ip.json
-	cp dns/ip.json ip.auto.tfvars.json
-	$(terraform) apply -auto-approve
+	$(call assertEnv, DOMAIN_NAME)
+	$(terraform) apply -auto-approve -var 'parent_zone=$(DOMAIN_NAME)'
 	$(terraform) output -json > host.json
 
 uninstall: $(terraform) .terraform/plugins/init ##: Remove any deployed infrastructure
@@ -16,9 +14,9 @@ uninstall: $(terraform) .terraform/plugins/init ##: Remove any deployed infrastr
 	$(terraform) destroy -auto-approve
 
 # Download plugins for each resource provider
-.terraform/plugins/initialized.state: $(terraform) providers.tf
+.terraform/plugins/init: $(terraform) providers.tf
 	$(terraform) init
 	@touch $@
 
 clean: ##: Remove any purely-local assets (not terraform state)
-	rm -rf .terraform ami.auto.tfvars.json host.json
+	rm -rf .terraform ami.auto.tfvars.json host.json ami.auto.tfvars.json

infrastructure/host.tf
@@ -1,11 +1,7 @@
-output "zoolander" {
-  value = aws_instance.zoolander.public_ip
-}
-
 resource "aws_instance" "zoolander" {
-  ami             = var.ami.value
-  instance_type   = "t2.medium"
-  security_groups = [aws_security_group.zoolander.name]
+  ami             = data.aws_ami.zoolander_latest.id
+  instance_type   = "t2.small"
+  security_groups = [local.network.security_group]
   key_name        = aws_key_pair.zoolander.key_name
 
   tags = {
@@ -23,6 +19,7 @@ resource "aws_instance" "zoolander" {
 
   provisioner "remote-exec" {
     inline = [
+      format("hostname -s %s", local.network.dns),
       "svcadm enable svc:/network/http",
       "git init zoolander"
     ]
@@ -47,40 +44,10 @@ resource "aws_instance" "zoolander" {
   }
 }
 
-variable "ami" {}
-
-resource "aws_security_group" "zoolander" {
-  name        = "zoolander"
-  description = "SSH, HTTP, and outbound IP4 for Zoolander CI"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [join("/", [data.external.nat_ip.result.ip, 32])]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "zoolander"
-  }
-}
-
-data "external" "nat_ip" {
-  program = ["bash", "-c", "echo '{'\\\"ip\\\": \\\"`dig -4 @resolver1.opendns.com myip.opendns.com +short`\\\"'}'"]
+data "aws_ami" "zoolander_latest" {
+  most_recent = true
+  owners      = ["self"]
+  name_regex  = "^zoolander-*"
 }
 
 resource "aws_key_pair" "zoolander" {
@@ -88,9 +55,7 @@ resource "aws_key_pair" "zoolander" {
   public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
 }
 
-variable "ip" {}
-
 resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.zoolander.id
-  allocation_id = var.ip.value.id
+  allocation_id = local.network.ip_allocation
 }

infrastructure/image/Makefile
@@ -1,27 +1,17 @@
 include lib.mk
 
 packer=$(call which, packer)
-terraform=$(call which, terraform)
 jq=$(call which, jq)
 
-install: ami.json $(jq) ##: Build an updated AMI
-	$(jq) '.' $<
-
-# Machine-readable description of the most recently built AMI
-ami.json: $(terraform) .terraform/plugins/init ami.tf packed.state
-	$(call assertEnv, AWS_DEFAULT_REGION)
-	$(terraform) apply -auto-approve
-	$(terraform) output -json > $@
-
-# Download plugins for each resource provider
-.terraform/plugins/init: $(terraform) providers.tf
-	$(terraform) init
-	@touch $@
-
-# Checkpoint for a successful packer run
-packed.state: $(packer) vars.json packer.json provision.sh
+install: $(packer) vars.json packer.json provision.sh ##: Build an updated AMI
 	$(packer) build -var-file=vars.json packer.json
 	@touch $@
+
+vars.json: $(jq) omnios_by_region.json
+	$(call assertEnv, AWS_DEFAULT_REGION)
+	$(jq) \
+		'{region: "$(AWS_DEFAULT_REGION)", omnios_ami: ."'$(AWS_DEFAULT_REGION)'"}' \
+		omnios_by_region.json > $@
 
 vars.json: $(jq) omnios_by_region.json
 	$(call assertEnv, AWS_DEFAULT_REGION)
@@ -35,5 +25,5 @@ deregister_old: filter_old_image_ids.jq  ##: Clean up any old images
 		| jq -rf $< \
 		| xargs -n1 aws ec2 deregister-image --image-id
 
-clean: ##: Remove any purely-local assets (not Terraform state)
-	rm -rf ami.json *.state .terraform/
+clean: ##: Remove any purely-local assets (not images)
+	rm -rf vars.json

infrastructure/dns/Makefile
@@ -1,23 +1,0 @@
-include lib.mk
-
-terraform=$(call which, terraform)
-jq=$(call which, jq)
-
-install: ip.json $(jq) ##: Create an Elastic IP resource
-	$(jq) '.' $<
-
-remove: $(terraform) .terraform/plugins/init ##: Release the Elastic IP back to Amazon
-	$(call assertEnv, AWS_DEFAULT_REGION)
-	$(terraform) destroy -auto-approve
-	rm -f ip.json
-
-# Machine-readable description of current Elastic IP
-ip.json: $(terrafom) .terraform/plugins/init ip.tf
-	$(call assertEnv, AWS_DEFAULT_REGION)
-	$(terraform) apply -auto-approve
-	$(terraform) output -json > $@
-	
-# Download plugins for each resource provider
-.terraform/plugins/init: $(terraform) providers.tf
-	$(terraform) init
-	@touch $@

infrastructure/dns/.gitignore
@@ -1,2 +1,0 @@
-.terraform/*
-ip.json

infrastructure/dns/ip.tf
@@ -1,11 +1,0 @@
-resource "aws_eip" "ip" {
-  vpc = false
-}
-
-output "ip" {
-  value = {
-    id  = aws_eip.ip.id
-    ip  = aws_eip.ip.public_ip
-    dns = aws_eip.ip.public_dns
-  }
-}

infrastructure/dns/lib.mk
@@ -1,1 +1,0 @@
-../lib.mk

infrastructure/dns/providers.tf
@@ -1,1 +1,0 @@
-provider "aws" {}

commit b38cf0eca5bf15223defecbf9a931c61c5f55794

Fix helper utilities broken in 08095d124da6d38275ddcf937b7e828239467512

Makefile
@@ -41,4 +41,4 @@ rotate: remote.txt ##: Rotate your instance's API tokens
 .PHONY: remote.txt
 remote.txt: $(jq)
 	$(MAKE) -C infrastructure install
-	$(jq) -r '"root@" + .zoolander.value + ":zoolander"' infrastructure/host.json > $@
+	$(jq) -r '"root@" + .domain_name + ":zoolander"' infrastructure/host.json > $@

infrastructure/Makefile
@@ -3,15 +3,19 @@ include lib.mk
 terraform=$(call which, terraform)
 jq=$(call which, jq)
 
-install: $(terraform) .terraform/plugins/init ##: Deploy latest infrastructure
+install: host.json ##: Deploy latest infrastructure
+
+.PHONY: host.json
+host.json: $(terraform) .terraform/plugins/init
 	$(call assertEnv, AWS_DEFAULT_REGION)
 	$(call assertEnv, DOMAIN_NAME)
 	$(terraform) apply -auto-approve -var 'parent_zone=$(DOMAIN_NAME)'
-	$(terraform) output -json > host.json
+	$(terraform) output -json host > $@
 
 uninstall: $(terraform) .terraform/plugins/init ##: Remove any deployed infrastructure
 	$(call assertEnv, AWS_DEFAULT_REGION)
-	$(terraform) destroy -auto-approve
+	$(call assertEnv, DOMAIN_NAME)
+	$(terraform) destroy -auto-approve -var 'parent_zone=$(DOMAIN_NAME)'
 
 # Download plugins for each resource provider
 .terraform/plugins/init: $(terraform) providers.tf

infrastructure/host.tf
@@ -1,3 +1,10 @@
+output "host" {
+  value = {
+    domain_name = local.network.dns
+    ip_address  = aws_instance.zoolander.public_ip
+  }
+}
+
 resource "aws_instance" "zoolander" {
   ami             = data.aws_ami.zoolander_latest.id
   instance_type   = "t2.small"

infrastructure/image/Makefile
@@ -13,6 +13,12 @@ vars.json: $(jq) omnios_by_region.json
 		'{region: "$(AWS_DEFAULT_REGION)", omnios_ami: ."'$(AWS_DEFAULT_REGION)'"}' \
 		omnios_by_region.json > $@
 
+vars.json: $(jq) omnios_by_region.json
+	$(call assertEnv, AWS_DEFAULT_REGION)
+	$(jq) \
+		'{region: "$(AWS_DEFAULT_REGION)", omnios_ami: ."'$(AWS_DEFAULT_REGION)'"}' \
+		omnios_by_region.json > $@
+
 deregister_old: filter_old_image_ids.jq  ##: Clean up any old images
 	$(call assertEnv, AWS_DEFAULT_REGION)
 	aws ec2 describe-images --owners self \