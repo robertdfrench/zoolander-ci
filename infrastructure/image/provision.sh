c10252bf9f4c11dbb25a4c4159f76722e87a68ed
Robert D French <robert@robertdfrench.me>
2019-07-15 23:02:47 -0400

    Install dehydrated

--- infrastructure/image/provision.sh
+++ infrastructure/image/provision.sh
@@ -2,3 +2,7 @@
 pkg install git build-essential zip
 pkg set-publisher -g https://pkg.omniosce.org/r151030/extra/ extra.omnios
 pkg install rust nginx-116
+curl --silent \
+	https://raw.githubusercontent.com/lukas2511/dehydrated/master/dehydrated \
+	> /usr/bin/dehydrated \
+	&& chmod +x /usr/bin/dehydrated
